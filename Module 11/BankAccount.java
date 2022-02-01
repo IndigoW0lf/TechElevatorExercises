061d51610f3c5155aa566b0337ca253ce709b041
Kai Indigo Wolf <kai.indigo.wolf@gmail.com>
2022-01-31 20:27:27 -0600

    Complete Module 11 BankAccount Exercise

---
+++ Module 11/BankAccount.java
@@ -0,0 +1,48 @@
+package com.techelevator;
+
+public class BankAccount {
+
+    //instance variables
+    private String accountHolderName;
+    private String accountNumber;
+    private int balance;
+
+    //constructor w/ two parameters
+    public BankAccount(String accountHolderName, String accountNumber) {
+        this.accountHolderName = accountHolderName;
+        this.accountNumber = accountNumber;
+    }
+
+    public BankAccount(String accountHolderName, String accountNumber, int balance) {
+        this.accountHolderName = accountHolderName;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    //methods
+    //balance = amountToDeposit + balance
+    //balance = balance - amountToWithdraw
+    public int deposit(int amountToDeposit) {
+        balance = balance + amountToDeposit;
+        return balance;
+    }
+
+    public int withdraw(int amountToWithdraw) {
+        balance = balance -amountToWithdraw;
+        return balance;
+    }
+
+    //getter or setters?
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+}
---
+++ Module 11/CheckingAccount.java
@@ -0,0 +1,35 @@
+package com.techelevator;
+
+
+public class CheckingAccount extends BankAccount{
+    //instance variables inherited
+    //methods inherited
+
+    //constructors
+    public CheckingAccount(String accountHolderName, String accountNumber) {
+        super(accountHolderName, accountNumber);
+    }
+
+    public CheckingAccount(String accountHolderName, String accountNumber, int balance) {
+        super(accountHolderName, accountNumber, balance);
+    }
+
+    @Override
+    public int withdraw(int amountToWithdraw) {
+        if(super.getBalance() >= amountToWithdraw) {
+            super.withdraw(amountToWithdraw);
+        }
+        else {
+            if(super.getBalance() - amountToWithdraw >= -100) {
+                super.withdraw(amountToWithdraw + 10);
+            }
+            else {
+                return super.getBalance();
+            }
+        }
+
+        return super.getBalance();
+    }
+
+    //getters/setters inherited
+}
